980a743913c44900897272fa899efc032df315d0
morioka <sou.kokomirai@gmail.com>
2024-03-30 19:44:42 +0900

    プロンプトを修正

--- lib/chat_api.dart
+++ lib/chat_api.dart
@@ -6,7 +6,7 @@ class ChatService {
   final String? apiKey = dotenv.env['ChatGPTKEY'];
 
   String generateEnglishPrompt(String theme, String difficulty) {
-    return "あなたはプロのライターです。以下の【テーマ】について記事を生成してください。【条件】【目的】【形式】の内容を踏まえて、英語で{英語本文}を作成してください。\n#テーマ\n$theme\n#条件\n- $difficulty\n- 読者の知的レベル:高校生\n- 語数:50語程度\n#目的\n- 英語学習\n- 教養を身につけるため\n#形式\n- 英文の始まりにはタイトルをつけず、いきなり本文から始めること";
+    return "あなたはプロのライターです。以下の【テーマ】について記事を生成してください。【条件】【目的】【形式】の内容を踏まえて、英語で{英語本文}を作成してください。\n#テーマ\n$theme\n#条件\n- CEFR:$difficulty\n- 読者の知的レベル:高校生\n- 語数:50語程度\n#目的\n- 英語学習\n- 教養を身につけるため\n#形式\n- 英文の始まりにはタイトルをつけず、いきなり本文から始めること";
   }
 
   String generateJapanesePrompt(String englishText) {
